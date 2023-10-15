d60420e0b712bc861d4ce39a62be8c81e743d909
NNS1116 <143465769+NNS1116@users.noreply.github.com>
2023-10-15 13:51:10 -0400

    Initial commit

---
+++ ArabicMVC/Controllers/TextController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ArabicMVC.Models;
+
+
+public class TextController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult ReverseText(TextModel model)
+    {
+        if (model.ArabicText == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        var reversedText = new string(model.ArabicText.Reverse().ToArray());
+        ViewData["ReversedText"] = reversedText;
+        return View("Index");
+    }
+
+    [HttpPost]
+    public IActionResult CountWords(TextModel model)
+    {
+        if (model.ArabicText == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        var words = model.ArabicText.Split(new char[] { ' ', '\t', '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
+        ViewData["WordCount"] = words.Length;
+        return View("Index");
+    }
+
+    [HttpPost]
+    public IActionResult CountLetters(TextModel model)
+    {
+        if (model.ArabicText == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        var letterCount = model.ArabicText.Count(char.IsLetter);
+        ViewData["LetterCount"] = letterCount;
+        return View("Index");
+    }
+}
---
+++ ArabicMVC/Models/TextModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArabicMVC.Models
+{
+    public class TextModel
+    {
+        public string ArabicText { get; set; }
+}
+}
